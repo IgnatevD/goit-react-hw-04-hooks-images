556ca237c2626e0a5e7f9b7d0c75a5ace583dbed
Ignatev Dmitryi <ignatevds@gmail.com>
2022-04-27 12:47:19 +0300

    push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import App from './components/App';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import './i18n';
-import reportWebVitals from './reportWebVitals';
+/** @format */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react"
+
+import App from "./Components/App"
+import ReactDOM from "react-dom/client"
+import "./index.scss"
+import "./i18n"
+import reportWebVitals from "./reportWebVitals"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
 
-reportWebVitals();
+reportWebVitals()
