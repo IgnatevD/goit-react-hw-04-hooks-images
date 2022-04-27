4bcf0db13cd7d35a2afbc54e51fee68ce7a77344
Ignatev Dmitryi <ignatevds@gmail.com>
2022-04-27 16:21:40 +0300

    удаление не нужных стилией

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,9 @@
-import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import { initReactI18next } from 'react-i18next';
+/** @format */
+
+import i18n from "i18next"
+import Backend from "i18next-xhr-backend"
+import LanguageDetector from "i18next-browser-languagedetector"
+import { initReactI18next } from "react-i18next"
 
 i18n
   // load translation using xhr -> see /public/locales
@@ -27,12 +29,12 @@ i18n
         }
       }
     },*/
-    fallbackLng: 'en',
+    fallbackLng: "en",
     debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-  });
+  })
 
-export default i18n;
+export default i18n
