2c1a4482c7eb93b24c5eb1f0b546b2aa2d66edf3
Ignatev Dmitryi <ignatevds@gmail.com>
2022-04-29 00:37:25 +0300

    fixed style header

--- src/Components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/Components/Header/BurgerMenu/BurgerMenu.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import Button from '../../Button';
-import Svg from '../../Svg';
-import Menu from '../Menu';
+/** @format */
 
-import styles from './BurgerMenu.module.scss';
+import React, { useEffect, useState } from "react"
+import Button from "../../Button"
+import Svg from "../../Svg"
+import Menu from "../Menu"
+
+import styles from "./BurgerMenu.module.scss"
 
 const BurgerMenu = () => {
-  const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false);
+  const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false)
 
   useEffect(() => {
     if (!isBurgerMenuOpen) {
-      document.body.style.overflow = '';
+      document.body.style.overflow = ""
     }
     if (isBurgerMenuOpen) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden"
     }
-  }, [isBurgerMenuOpen]);
+  }, [isBurgerMenuOpen])
 
-  const onToggleModal = () => setBurgerMenuOpen(prev => !prev);
+  const onToggleModal = () => setBurgerMenuOpen((prev) => !prev)
 
   return (
     <div>
       <Button className={styles.btn__home} onClick={onToggleModal}>
-        <Svg icon="#icon-menu" className={styles.menu} />
+        <Svg icon='#icon-menu' className={styles.menu} />
       </Button>
 
       {isBurgerMenuOpen && (
         <div className={styles.menu__container}>
           <Button className={styles.btn__close} onClick={onToggleModal}>
-            <Svg icon="#icon-close" />
+            <Svg icon='#icon-close' className={styles.icon__close} />
           </Button>
           <Menu onToggleModal={onToggleModal} />
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default BurgerMenu;
+export default BurgerMenu
